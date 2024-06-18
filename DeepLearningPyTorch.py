afee883c4f3cd7d2e39acd4e443daf6c6e85ec04
DimaGirya <DimaGirya1990@gmail.com>
2024-06-18 23:23:57 +0300

    fix of score

--- DeepLearningPyTorch.py
+++ DeepLearningPyTorch.py
@@ -79,7 +79,7 @@ def InitWeightsKaiNorm( oLayer: nn.Module, tuLyrClas: Tuple = (nn.Linear, nn.Con
         nn.init.kaiming_normal_(oLayer.weight.data)
 
 
-def RunEpoch( oModel: nn.Module, dlData: DataLoader, hL: Callable, hS: Callable, oOpt: Optional[Optimizer] = None, opMode: NNMode = NNMode.TRAIN ) -> Tuple[float, float]:
+def RunEpoch(oModel: nn.Module, dlData: DataLoader, hL: Callable, hS: Callable, oOpt: Optional[Optimizer] = None, opMode: NNMode = NNMode.TRAIN) -> Tuple[float, float]:
     """
     Runs a single Epoch (Train / Test) of a model.  
     Input:
@@ -101,82 +101,90 @@ def RunEpoch( oModel: nn.Module, dlData: DataLoader, hL: Callable, hS: Callable,
       - The optimizer is required for training mode.
     """
     
-    epochLoss   = 0.0
-    epochScore  = 0.0
-    numSamples  = 0
+    epochLoss = 0.0
+    epochScore = 0.0
+    numSamples = 0
     numBatches = len(dlData)
-
-    runDevice = next(oModel.parameters()).device #<! CPU \ GPU
+    start = time.time()
+    runDevice = next(oModel.parameters()).device  # Get the device (CPU/GPU) where the model is located
 
     if opMode == NNMode.TRAIN:
-        oModel.train(True) #<! Equivalent of `oModel.train()`
+        oModel.train(True)
     elif opMode == NNMode.INFERENCE:
-        oModel.eval() #<! Equivalent of `oModel.train(False)`
+        oModel.eval()
     else:
         raise ValueError(f'The `opMode` value {opMode} is not supported!')
-    
+
     for ii, (mX, vY) in enumerate(dlData):
-        # Move Data to Model's device
-        mX = mX.to(runDevice) #<! Lazy
-        vY = vY.to(runDevice) #<! Lazy
+        start_epcoh = time.time()
+        mX = mX.to(runDevice)
+        vY = vY.to(runDevice)
 
         batchSize = mX.shape[0]
-        
+
         if opMode == NNMode.TRAIN:
-            # Forward
-            mZ      = oModel(mX) #<! Model output
-            valLoss = hL(mZ, vY) #<! Loss
-            
-            # Backward
-            oOpt.zero_grad()    #<! Set gradients to zeros
-            valLoss.backward()  #<! Backward
-            oOpt.step()         #<! Update parameters
-        else: #<! Value of `opMode` was already validated
+            mZ = oModel(mX)
+            valLoss = hL(mZ, vY)
+
+            oOpt.zero_grad()
+            valLoss.backward()
+            oOpt.step()
+        else:
             with torch.no_grad():
-                # No computational graph
-                mZ      = oModel(mX) #<! Model output
-                valLoss = hL(mZ, vY) #<! Loss
+                mZ = oModel(mX)
+                valLoss = hL(mZ, vY)
 
         with torch.no_grad():
-            # Score
-            valScore = hS(mZ, vY)
-            # Normalize so each sample has the same weight
-            epochLoss  += batchSize * valLoss.item()
+            # Convert vY and mZ to index tensors if they are not already
+            vY_index = vY.argmax(dim=1) if vY.dtype == torch.float64 else vY
+            mZ_index = mZ.argmax(dim=1) if mZ.dtype == torch.float32 else mZ
+
+            #print(f"Shape of mZ_index: {mZ_index.shape}, dtype: {mZ_index.dtype}")
+            #print(f"Shape of vY_index: {vY_index.shape}, dtype: {vY_index.dtype}")
+            
+            try:
+                valScore = hS(mZ_index, vY_index)
+                #print(f"Shape of preds: {mZ_index.shape}, dtype: {mZ_index.dtype}")
+                #print(f"Shape of target: {vY_index.shape}, dtype: {vY_index.dtype}")
+            except Exception as e:
+                print(f"Error during scoring: {e}")
+                raise e
+
+            epochLoss += batchSize * valLoss.item()
             epochScore += batchSize * valScore.item()
             numSamples += batchSize
+            epochTime = time.time() - start_epcoh
 
-        print(f'\r{"Train" if opMode == NNMode.TRAIN else "Val"} - Iteration: {(ii + 1):3d} / {numBatches}: loss = {valLoss:.6f}', end = '')
-    
-    print('', end = '\r')
-            
+        print(f'{"Train" if opMode == NNMode.TRAIN else "Val"} - Iteration: {(ii + 1):3d} / {numBatches}: loss = {valLoss:.6f} epoch time:{epochTime:5.2f}')
+    total_time = time.time() - start
+    print('', end='\r')
+    print(f'{"Train" if opMode == NNMode.TRAIN else "Val"} - Total Time:{total_time:5.2f} |', end='')
+
     return epochLoss / numSamples, epochScore / numSamples
+
 
 # Training Model Loop Function
 
-def TrainModel( oModel: nn.Module, dlTrain: DataLoader, dlVal: DataLoader, oOpt: Optimizer, numEpoch: int, hL: Callable, hS: Callable, oSch: Optional[LRScheduler] = None, oTBWriter: Optional[SummaryWriter] = None) -> Tuple[nn.Module, List, List, List, List]:
-
-    lTrainLoss  = []
+def TrainModel(oModel: nn.Module, dlTrain: DataLoader, dlVal: DataLoader, oOpt: Optimizer, numEpoch: int, hL: Callable, hS: Callable, oSch: Optional[LRScheduler] = None, oTBWriter: Optional[SummaryWriter] = None) -> Tuple[nn.Module, List, List, List, List]:
+    lTrainLoss = []
     lTrainScore = []
-    lValLoss    = []
-    lValScore   = []
-    lLearnRate  = []
+    lValLoss = []
+    lValScore = []
+    lLearnRate = []
 
-    # Support R2
-    bestScore = -1e9 #<! Assuming higher is better
+    bestScore = -1e9
 
     learnRate = oOpt.param_groups[0]['lr']
 
     for ii in range(numEpoch):
-        startTime           = time.time()
-        trainLoss, trainScr = RunEpoch(oModel, dlTrain, hL, hS, oOpt, opMode = NNMode.TRAIN) #<! Train
-        valLoss,   valScr   = RunEpoch(oModel, dlVal, hL, hS, oOpt, opMode = NNMode.INFERENCE) #<! Score Validation
+        startTime = time.time()
+        trainLoss, trainScr = RunEpoch(oModel, dlTrain, hL, hS, oOpt, opMode = NNMode.TRAIN)
+        valLoss, valScr = RunEpoch(oModel, dlVal, hL, hS, oOpt, opMode = NNMode.INFERENCE)
         if oSch is not None:
-            # Adjusting the scheduler on Epoch level
             learnRate = oSch.get_last_lr()[0]
             oSch.step()
-        epochTime           = time.time() - startTime
+        epochTime = time.time() - startTime
 
-        # Aggregate Results
         lTrainLoss.append(trainLoss)
         lTrainScore.append(trainScr)
         lValLoss.append(valLoss)
@@ -187,16 +195,14 @@ def TrainModel( oModel: nn.Module, dlTrain: DataLoader, dlVal: DataLoader, oOpt:
             oTBWriter.add_scalars('Loss (Epoch)', {'Train': trainLoss, 'Validation': valLoss}, ii)
             oTBWriter.add_scalars('Score (Epoch)', {'Train': trainScr, 'Validation': valScr}, ii)
             oTBWriter.add_scalar('Learning Rate', learnRate, ii)
-        
-        # Display (Babysitting)
-        print('Epoch '              f'{(ii + 1):4d} / ' f'{numEpoch}:', end = '')
-        print(' | Train Loss: '     f'{trainLoss          :6.3f}', end = '')
-        print(' | Val Loss: '       f'{valLoss            :6.3f}', end = '')
-        print(' | Train Score: '    f'{trainScr           :6.3f}', end = '')
-        print(' | Val Score: '      f'{valScr             :6.3f}', end = '')
-        print(' | Epoch Time: '     f'{epochTime          :5.2f}', end = '')
 
-        # Save best model ("Early Stopping")
+        print('Epoch ' f'{(ii + 1):4d} / ' f'{numEpoch}:', end='')
+        print(' | Train Loss: ' f'{trainLoss:6.3f}', end='')
+        print(' | Val Loss: ' f'{valLoss:6.3f}', end='')
+        print(' | Train Score: ' f'{trainScr:6.3f}', end='')
+        print(' | Val Score: ' f'{valScr:6.3f}', end='')
+        print(' | Epoch Time: ' f'{epochTime:5.2f}', end='')
+
         if valScr > bestScore:
             bestScore = valScr
             try:
@@ -204,16 +210,17 @@ def TrainModel( oModel: nn.Module, dlTrain: DataLoader, dlVal: DataLoader, oOpt:
                 if oSch is not None:
                     dCheckPoint['Scheduler'] = oSch.state_dict()
                 torch.save(dCheckPoint, 'BestModel.pt')
-                print(' | <-- Checkpoint!', end = '')
+                print(' | <-- Checkpoint!', end='')
             except:
-                print(' | <-- Failed!', end = '')
+                print(' | <-- Failed!', end='')
         print(' |')
-    
+
     # Load best model ("Early Stopping")
     # dCheckPoint = torch.load('BestModel.pt')
     # oModel.load_state_dict(dCheckPoint['Model'])
 
     return oModel, lTrainLoss, lTrainScore, lValLoss, lValScore, lLearnRate
+
 
 
 # Auxiliary Blocks
