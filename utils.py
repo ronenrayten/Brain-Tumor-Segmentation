fc6fee4492af8de6d6ca4834ceb63f308ed3771f
DimaGirya <DimaGirya1990@gmail.com>
2024-06-29 17:49:00 +0300

    working on visualization

--- utils.py
+++ utils.py
@@ -1,2 +1,24 @@
+import os
+
+from unet3d import UNet3D
+import torch
+
 def str_to_bool(s):
     return s.lower() in ('true', '1')
+
+def create_dir_if_not_exists(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+def load_model(model_path):
+    # Load the trained model
+    model = UNet3D(in_channels=4, num_classes=4)  # Ensure in_channels is set to 4 to match the checkpoint
+
+    # Load the entire checkpoint
+    checkpoint = torch.load(model_path)
+    # Ensure the keys in the checkpoint match the model state_dict
+    print("Checkpoint keys:", checkpoint.keys())
+
+    # Extract only the model state_dict
+    model.load_state_dict(checkpoint['Model'])
+    model.eval()
+    return model
--- visualization.py
+++ visualization.py
@@ -6,7 +6,7 @@ import torch
 from datasets import ImageDatasetFromDisk
 
 
-def visualization_model_result_as_2d(model, sample_id):
+def visualization_model_result_as_2d(model, sample_id, z_slice=64, file_path=None):
     batchSize = 1
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -18,13 +18,8 @@ def visualization_model_result_as_2d(model, sample_id):
     dlTrain = torch.utils.data.DataLoader(dsTrain, shuffle=True, batch_size=1 * batchSize, drop_last=True)
     tX, vY = next(iter(dlTrain))  # <! PyTorch Tensors
 
-    # Check the shape of the tensor before passing to model
-    print("Shape of input tensor:", tX.shape)
-    print("Shape of random label:", vY.shape)
-
     # Convert random_label to single-channel by taking argmax along the channel dimension
     random_label = vY.squeeze()
-
     # Run the example through the model
     mode = model.to(device)
     tX = tX.to(device)
@@ -39,12 +34,10 @@ def visualization_model_result_as_2d(model, sample_id):
     output = output.cpu()
     output = output.argmax(dim=1).squeeze()  # Get the class with the highest score
     random_label = np.argmax(random_label, axis=0)
-    print('unique labels:', np.unique(random_label))
-    print("Random label shape after squeeze:", random_label.shape)
     output = output.numpy()
 
-    output_slice = output[:, :, 64]  # Adjust indexing to ensure correct slicing
-    label_slice = random_label[:, :, 64]  # Ensure the label slice is correctly selected
+    output_slice = output[:, :, z_slice]  # Adjust indexing to ensure correct slicing
+    label_slice = random_label[:, :, z_slice]  # Ensure the label slice is correctly selected
 
     # Define a colormap for different classes
     cmap = ListedColormap(['black', 'red', 'green', 'blue'])
@@ -56,7 +49,7 @@ def visualization_model_result_as_2d(model, sample_id):
     # Original image
     plt.subplot(1, 3, 1)
     plt.title('Original Image')
-    plt.imshow(tX[0, 0, :, :, 64], cmap='gray')  # Adjust the slice for visualization
+    plt.imshow(tX[0, 0, :, :, z_slice], cmap='gray')  # Adjust the slice for visualization
 
     # Original label
     plt.subplot(1, 3, 2)
@@ -68,9 +61,8 @@ def visualization_model_result_as_2d(model, sample_id):
     plt.title('Model Output')
     plt.imshow(output_slice, cmap=cmap, interpolation='nearest')  # Use the discrete colormap
 
-    plt.show()
-
-    print('output labels', np.unique(output_slice))
-    print('output labels', np.unique(label_slice))
-    print(output_slice.shape)
-    print(label_slice.shape)
+    if file_path:
+        plt.savefig(file_path)
+        print()
+    else:
+        plt.show()
